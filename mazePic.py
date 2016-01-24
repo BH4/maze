f987369ddf7ad714363cc8099d9bc7fc09672576
BH4 <brycefore4@gmail.com>
2016-01-23 18:42:51 -0800

    Added the ability to solve maze and plot the solution
    Only breadth first solver is finished. In the future maybe create gifs
    of the different types of algorithms solving the maze.

--- mazePic.py
+++ mazePic.py
@@ -2,7 +2,7 @@ from PIL import Image
 
 class mazePic:
     def __init__(self,maze):
-        self.wallThickness=1#cant change this yet
+        self.wallThickness=1#cant change this yet everything is written so that it moves correctly but the walls are still only 1 thick
 
         #desired image max(width,height)
         desired=500
@@ -23,6 +23,9 @@ class mazePic:
     def savePic(self,name):
         self.im.save(name+'.jpg', "JPEG")
 
+
+    #----------------------------------------------------
+    #drawing the maze walls
     def drawMaze(self,walls):
         for i in xrange(len(walls)):#vert or horiz
             lwi=len(walls[i])
@@ -33,25 +36,28 @@ class mazePic:
                         self.drawWall((i,j,k))
 
     def drawWall(self,coord):
+        self.coloredWall(coord,(0,0,0))
+
+    def coloredWall(self,coord,color):
         (x1,x2,y1,y2)=self.wallEndPoints(coord)
         if coord[0]==0:
-            self.drawVertLine(x1,y1,y2)
+            self.drawVertLine(x1,y1,y2,color)
         if coord[0]==1:
-            self.drawHorLine(x1,x2,y1)
+            self.drawHorLine(x1,x2,y1,color)
 
-    def drawVertLine(self,x,y1,y2):
+    def drawVertLine(self,x,y1,y2,color):
         dy=abs(y2-y1)
         y=min(y1,y2)
 
         for i in xrange(dy+2):#making this +2 means some lines will overlap but there wont be any white spots
-            self.pix[x,y+i]=(0,0,0)
+            self.pix[x,y+i]=color
         
-    def drawHorLine(self,x1,x2,y):
+    def drawHorLine(self,x1,x2,y,color):
         dx=abs(x2-x1)
         x=min(x1,x2)
 
         for i in xrange(dx+2):
-            self.pix[x+i,y]=(0,0,0)
+            self.pix[x+i,y]=color
 
 
     def wallEndPoints(self,coord):
@@ -67,3 +73,63 @@ class mazePic:
             x2=x1+self.cellSize
 
         return (x1,x2,y1,y2)
+    #-------------------------------------------------------
+
+    #given two cells find the coordinates of the wall between them
+    def findWall(self,pos1,pos2):
+        if pos1==pos2:
+            print 'same point'
+            return None
+        
+        dx=pos1[0]-pos2[0]
+        dy=pos1[1]-pos2[1]
+        if abs(dx)+abs(dy)>1:
+            print 'not connected'
+            return None
+
+        #at this point either (dx=+1 or -1 and dy=0) or (dy=+1 or -1 and dx=0)
+
+        #wall vertical or horizontal?
+        if dx==0:#horizonatal
+            wVorH=1
+
+            x=pos1[0]
+            y=max(pos1[1],pos2[1])
+        if dy==0:#vertical
+            wVorH=0
+
+            x=max(pos1[0],pos2[0])
+            y=pos1[1]
+
+        return (wVorH,x,y)
+
+    #find top left corner of cell (corners are inside the cell not on wall)
+    #cood = (x,y)
+    def corner(self,coord):
+        x=self.wallThickness*(coord[0]+1)+self.cellSize*coord[0]
+        y=self.wallThickness*(coord[1]+1)+self.cellSize*coord[1]
+
+        return (x,y)
+
+    #fill squre with color
+    def fill(self,coord,color):
+        (x,y)=self.corner(coord)
+
+        for i in xrange(self.cellSize):
+            for j in xrange(self.cellSize):
+                self.pix[x+i,y+j]=color
+        
+
+    #color cells in specified color given in RGB format
+    def solution(self,path,color):
+        #path is list of coordinates
+        last=None
+        for cell in path:
+            #need to fill the space between the walls. Assumes path is in order
+            if not last is None:
+                wall=self.findWall(cell,last)
+                self.coloredWall(wall,color)
+            
+            self.fill(cell,color)
+
+            last=cell
