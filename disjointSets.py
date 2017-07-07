01b508566e5e762c164014c1e585bb816c641a4d
BH4 <brycefore4@gmail.com>
2017-07-07 15:51:00 -0700

    Added disjoint sets
    Made disjoint sets class. Doesn't use it yet though.

---
+++ disjointSets.py
@@ -0,0 +1,40 @@
+
+class disjointSets:
+    def __init__(self,n):
+        self.sets=[-1]*n
+        self.size=n
+
+    #returns true or false as to weather or not a union was preformed
+    def union(self,a,b):#union by size
+        rootA=self.find(a)
+        rootB=self.find(b)
+        
+        if rootA==rootB:
+            return False#union was not preformed
+
+        newSize=self.sets[rootA]+self.sets[rootB]#size is negative the number of elements of the set
+
+        #rootA has more elements
+        if self.sets[rootA]<self.sets[rootB]:
+            self.sets[rootB]=rootA
+            self.sets[rootA]=newSize
+        else:#B has more or same
+            self.sets[rootA]=rootB
+            self.sets[rootB]=newSize
+
+        self.size-=1
+
+        return True#union was preformed
+    def find(self,a):#finds root of element with id 'a'
+        #also make the parents of this node the root in order to decrease tree height
+
+        parent=self.sets[a]
+        if parent<0:
+            return a
+        
+        root=self.find(parent)
+
+        self.sets[a]=root
+
+        return root
+            
